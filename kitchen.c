c91b9ee378a9d99a5027312a3199f8e5b627d52b
mike bradshaw <ymbrads+github@gmail.com>
2013-03-06 11:23:12 +0200

    Update kitchen.c
    updated to pass in "karma" so it can be updated

--- kitchen.c
+++ kitchen.c
@@ -7,14 +7,14 @@ int main(int argc, char **argv)
 {
     int karma = 0;
 
-    karma += wipeSurfaces();
+    karma += wipeSurfaces(karma);
 
     if ( coffeeMachine == PROBLEM ) {
         solveErrorMessage();
     }
 
     if ( coffeeMachineMilk != InFridge ){
-        karma += returnMilkToFridge();
+        karma += returnMilkToFridge(karma);
     }
 
     return 0;
